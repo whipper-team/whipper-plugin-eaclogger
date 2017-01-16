whipper-team/whipper

commit 976e1bdca2f0f86b97cf0250b6d92beeee2bcb03

remove configure.configure, use __version__, remove getRevision() (#94)
* remove configure.configure, use __version__, remove getRevision()

* remove empty configure/ directory

* remove morituri.configure imports

setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
 from os import system, makedirs, environ
 
+import morituri
+
 setup(
     name="whipper",
-    version="0.4.0",
+    version=morituri.__version__,
     description="a secure cd ripper preferring accuracy over speed",
     author=['Thomas Vander Stichele', 'Joe Lametta', 'Samantha Baldwin'],
     maintainer=['Joe Lametta', 'Samantha Baldwin'],

morituri/__init__.py
@@ -2,6 +2,8 @@ import logging
 import os
 import sys
 
+__version__ = '0.4.0'
+
 level = logging.WARNING
 if 'WHIPPER_DEBUG' in os.environ:
     level = os.environ['WHIPPER_DEBUG'].upper()

morituri/command/main.py
@@ -6,10 +6,11 @@ import sys
 import pkg_resources
 import musicbrainzngs
 
+import morituri
+
 from morituri.command import cd, offset, drive, image, accurip, debug
 from morituri.command.basecommand import BaseCommand
 from morituri.common import common, directory
-from morituri.configure import configure
 from morituri.extern.task import task
 
 import logging
@@ -17,7 +18,7 @@ logger = logging.getLogger(__name__)
 
 def main():
     # set user agent
-    musicbrainzngs.set_useragent("whipper", configure.version,
+    musicbrainzngs.set_useragent("whipper", morituri.__version__,
                                  "https://github.com/JoeLametta/whipper")
     # register plugins with pkg_resources
     distributions, _ = pkg_resources.working_set.find_plugins(
@@ -82,5 +83,5 @@ You can get help on subcommands by using the -h option to the subcommand.
             self.parser.print_help()
             sys.exit(0)
         if self.options.version:
-            print "whipper %s" % configure.version
+            print "whipper %s" % morituri.__version__
             sys.exit(0)

morituri/common/common.py
@@ -333,37 +333,3 @@ class VersionGetter(object):
             raise
 
         return version
-
-
-def getRevision():
-    """
-    Get a revision tag for the current git source tree.
-
-    Appends -modified in case there are local modifications.
-
-    If this is not a git tree, return the top-level REVISION contents instead.
-
-    Finally, return unknown.
-    """
-    topsrcdir = os.path.join(os.path.dirname(__file__), '..', '..')
-
-    # only use git if our src directory looks like a git checkout
-    # if you run git regardless, it recurses up until it finds a .git,
-    # which may be higher than your current source tree
-    if os.path.exists(os.path.join(topsrcdir, '.git')):
-
-        # always falls back to the current commit hash if no tags are found
-        status, describe = commands.getstatusoutput('git describe --all')
-        if status == 0:
-            if commands.getoutput('git diff-index --name-only HEAD --'):
-                describe += '-modified'
-
-            return describe
-
-    # check for a top-level REVISION file
-    path = os.path.join(topsrcdir, 'REVISION')
-    if os.path.exists(path):
-        revision = open(path).read().strip()
-        return revision
-
-    return '(unknown)'

morituri/configure/.gitignore
@@ -1,2 +1,0 @@
-installed.py
-uninstalled.py

morituri/configure/configure.py
@@ -1,15 +1,0 @@
-# -*- Mode: Python -*-
-# vi:si:et:sw=4:sts=4:ts=4
-
-import os
-
-from morituri.common import common
-
-config_dict = {
-    'revision': common.getRevision(),
-    'version': '0.4.0',
-}
-
-for key, value in config_dict.items():
-    dictionary = locals()
-    dictionary[key] = value

morituri/image/table.py
@@ -28,8 +28,9 @@ import copy
 import urllib
 import urlparse
 
+import morituri
+
 from morituri.common import common
-from morituri.configure import configure
 
 import logging
 logger = logging.getLogger(__name__)
@@ -555,7 +556,7 @@ class Table(object):
 
         assert self.hasTOC(), "Table does not represent a full CD TOC"
         lines.append('REM DISCID %s' % self.getCDDBDiscId().upper())
-        lines.append('REM COMMENT "%s %s"' % (program, configure.version))
+        lines.append('REM COMMENT "%s %s"' % (program, morituri.__version__))
 
         if self.catalog:
             lines.append("CATALOG %s" % self.catalog)

morituri/result/logger.py
@@ -1,8 +1,9 @@
 import time
 import hashlib
 
+import morituri
+
 from morituri.common import common
-from morituri.configure import configure
 from morituri.result import result
 
 
@@ -26,7 +27,7 @@ class MorituriLogger(result.Logger):
         # Ripper version
         # Only implemented in whipper (ripResult.logger)
         lines.append("Log created by: whipper %s (%s logger)" % (
-                    configure.version, ripResult.logger))
+                    morituri.__version__, ripResult.logger))
 
         # Rip date
         date = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime(epoch)).strip()

whipper-team/whipper-plugin-eaclogger

commit 533a26f9dc79f25794d414e0072571c339b6c275

Fix version numer retrieval in recent whipper It has been broken since https://github.com/JoeLametta/whipper/commit/976e1bdca2f0f86b97cf0250b6d92beeee2bcb03 (whipper release v0.4.1)

.travis.yml
@@ -14,5 +14,5 @@ install:
   - pip install flake8
 
 script:
-  - flake8 --max-complexity 15 eaclogger/logger/eac.py
+  - flake8 --max-complexity 20 eaclogger/logger/eac.py
   - python setup.py bdist_egg

eaclogger/logger/eac.py
@@ -2,7 +2,6 @@ import time
 import hashlib
 
 from morituri.common import common
-from morituri.configure import configure
 from morituri.result import result
 
 
@@ -52,13 +51,23 @@ class EacLogger(result.Logger):
 
         # Ripper version
         # ATM differs from EAC's typical log line
-        lines.append("morituri version %s" % configure.version)
+        isWhipper = False
+        try:
+            from morituri.configure import configure
+            lines.append("morituri version %s" % configure.version)
+        except ImportError:
+            import morituri
+            lines.append("whipper version %s" % morituri.__version__)
+            isWhipper = True
         lines.append("")
 
         # Rip date
         # ATM differs from EAC's typical log line
         date = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime(epoch)).strip()
-        lines.append("morituri extraction logfile from %s" % date)
+        if isWhipper:
+            lines.append("whipper extraction logfile from %s" % date)
+        else:
+            lines.append("morituri extraction logfile from %s" % date)
         lines.append("")
 
         # Artist / Album

setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="morituri-eaclogger",
-    version="0.2.1",
+    version="0.2.2",
     description="""morituri EAC-style logger""",
     author="superveloman",
+    maintainer="JoeLametta",
     packages=[
         'eaclogger',
         'eaclogger.logger'],