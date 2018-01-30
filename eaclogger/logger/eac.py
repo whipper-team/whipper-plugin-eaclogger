whipper-team/whipper-plugin-eaclogger

commit 983e1da208925dccd9933810019c44727111fe89

Logger restructuring
Removed bloat, added a bit of documentation, etc.

eaclogger/logger/eac.py
@@ -5,29 +5,30 @@ from morituri.common import common
 from morituri.configure import configure
 from morituri.result import result
 
-
 class EacLogger(result.Logger):
 
     _accuratelyRipped = 0
     _inARDatabase = 0
     _errors = False
 
+    # Overrides morituri's common implementation because EAC writes minutes
+    # value without zero padding it (EAC: %2d) vs (morituri: %02d)
     def _framesToMSF(self, frames):
-        # format specifically for EAC log; examples:
-        # 5:39.57
+        """Returns MSF representation of the provided frames value"""
+
         f = frames % common.FRAMES_PER_SECOND
         frames -= f
         s = (frames / common.FRAMES_PER_SECOND) % 60
         frames -= s * 60
         m = frames / common.FRAMES_PER_SECOND / 60
-
         return "%2d:%02d.%02d" % (m, s, f)
 
-    def _framesToHMSH(self, frames):
-        # format specifically for EAC log; examples:
-        # 0:00.00.70
-        # FIXME: probably depends on General EAC setting (display as frames)
-        # if this formats as frames or as hundreds of seconds
+    # Overrides morituri's common implementation because EAC writes hours
+    # value without zero padding it (EAC: %2d) vs (morituri: %02d)
+    # HMSF is used to represent pre-gaps' duration
+    def _framesToHMSF(self, frames):
+        """Returns HMSF representation of the provided frames value"""
+
         f = frames % common.FRAMES_PER_SECOND
         frames -= f
         s = (frames / common.FRAMES_PER_SECOND) % 60
@@ -35,70 +36,76 @@ class EacLogger(result.Logger):
         m = frames / common.FRAMES_PER_SECOND / 60
         frames -= m * 60
         h = frames / common.FRAMES_PER_SECOND / 60 / 60
-
-        # return "%2d:%02d:%02d.%02d" % (h, m, s, int((f / 75.0) * 100.0))
         return "%2d:%02d:%02d.%02d" % (h, m, s, f)
 
     def log(self, ripResult, epoch=time.time()):
+        """Returns big str: logfile joined text lines"""
+
         lines = self.logRip(ripResult, epoch=epoch)
-        return '\n'.join(lines)
+        return "\n".join(lines)
 
     def logRip(self, ripResult, epoch):
+        """Returns lines list"""
 
         lines = []
 
-        # global
-
-        # version string; FIXME
+        # Ripper version
+        # ATM differs from EAC's typical log line
         lines.append("morituri version %s" % configure.version)
         lines.append("")
 
+        # Rip date
+        # ATM differs from EAC's typical log line
         date = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime(epoch)).strip()
         lines.append("morituri extraction logfile from %s" % date)
         lines.append("")
 
-        # album
+        # Artist / Album
         lines.append("%s / %s" % (ripResult.artist, ripResult.title))
         lines.append("")
 
-        # drive
+        # Drive information
+        # Missing information about "Adapter" (not relevant on *nix?)
         lines.append(
-            "Used drive  : %s%s %s" % (
-                ripResult.vendor, ripResult.model, ripResult.release))
+            "Used drive  : %s%s" % (
+                ripResult.vendor, ripResult.model))
         lines.append("")
 
-        # Default for cdparanoia; every sector gets read at least twice
+        # Rip settings
         lines.append("Read mode               : Secure")
+        # Extra line (not included in EAC's logfiles)
         lines.append("Use cdparanoia mode     : Yes (cdparanoia %s)" % (
             ripResult.cdparanoiaVersion))
-        defeat = 'Unknown'
+        defeat = "Unknown"
         if ripResult.cdparanoiaDefeatsCache is True:
-            defeat = 'Yes'
+            defeat = "Yes"
         if ripResult.cdparanoiaDefeatsCache is False:
-            defeat = 'No'
+            defeat = "No"
         lines.append("Defeat audio cache      : %s" % defeat)
-        # Default for cdparanoia by virtue of having no C2 rip mode
         lines.append("Make use of C2 pointers : No")
-
         lines.append("")
-
         lines.append("Read offset correction                      : %d" %
                      ripResult.offset)
-        # Currently unsupported by "vanilla" cdparanoia
+        # Currently unsupported by unpatched cdparanoia
         lines.append("Overread into Lead-In and Lead-Out          : No")
-        # Default for cdparanoia
         lines.append("Fill up missing offset samples with silence : Yes")
-        # Default for cdparanoia
         lines.append("Delete leading and trailing silent blocks   : No")
-        # Default
         lines.append("Null samples used in CRC calculations       : Yes")
+        # Missing line "Used interface" (not relevant on *nix?)
+        # Extra line (not included in EAC's logfiles)
         lines.append("Gap detection                               : "
                      "cdrdao version %s" % ripResult.cdrdaoVersion)
         lines.append("Gap handling                                : "
                      "Appended to previous track")
         lines.append("")
+
+        # ATM differs from EAC's typical log line
         lines.append("Used output format       : %s" %
                      ripResult.profileName)
+        # Extra lines (not included in EAC's logfiles)
+        # Are these appropriate replacements of the following missing lines?
+        # ("Selected bitrate", "Quality", "Add ID3 tag"
+        # "Command line compressor", "Additional command line options")
         lines.append("GStreamer pipeline       : %s" %
                      ripResult.profilePipeline)
         lines.append("GStreamer version        : %s" %
@@ -108,8 +115,9 @@ class EacLogger(result.Logger):
         lines.append("Encoder plugin version   : %s" %
                      ripResult.encoderVersion)
         lines.append("")
+        lines.append("")
 
-        # toc
+        # TOC
         lines.append("TOC of the extracted CD")
         lines.append("")
         lines.append(
@@ -124,6 +132,7 @@ class EacLogger(result.Logger):
         except KeyError:
             pass
 
+        # If true include HTOA line in log's TOC
         if htoa and htoa.path:
             htoastart = htoa.absolute
             htoaend = table.getTrackEnd(0)
@@ -135,8 +144,10 @@ class EacLogger(result.Logger):
                     self._framesToMSF(htoalength),
                     htoastart, htoaend))
 
+        # For every track include information in the TOC
         for t in table.tracks:
             # FIXME: what happens to a track start over 60 minutes ?
+            # Answer: tested empirically, everything seems OK
             start = t.getIndex(1).absolute
             length = table.getTrackLength(t.number)
             end = table.getTrackEnd(t.number)
@@ -146,21 +157,10 @@ class EacLogger(result.Logger):
                     self._framesToMSF(start),
                     self._framesToMSF(length),
                     start, end))
-
+        lines.append("")
         lines.append("")
 
-        # per-track
-        duration = 0.0
-        for t in ripResult.tracks:
-            if not t.filename:
-                continue
-            lines.extend(self.trackLog(t))
-            lines.append('')
-            duration += t.testduration + t.copyduration
-
-        # global overview
-        # FIXME
-
+        # AccurateRip summary at the end of the logfile
         if self._inARDatabase == 0:
             lines.append("None of the tracks are present "
                          "in the AccurateRip database")
@@ -174,95 +174,121 @@ class EacLogger(result.Logger):
                     "You may have a different pressing "
                     "from the one(s) in the database")
             elif self._accuratelyRipped < nonHTOA:
-                lines.append("%d track(s) accurately ripped" %
-                             self._accuratelyRipped)
-                lines.append("%d track(s) could not be verified as accurate" %
-                             (nonHTOA - self._accuratelyRipped))
+                if self._accuratelyRipped < 10:
+                    lines.append(" %d track(s) accurately ripped" %
+                                 self._accuratelyRipped)
+                else:
+                    lines.append("%d track(s) accurately ripped" %
+                                 self._accuratelyRipped)
+                if (nonHTOA - self._accuratelyRipped) < 10:
+                    lines.append(" %d track(s) could not be verified "
+                                 "as accurate" %
+                                 (nonHTOA - self._accuratelyRipped))
+                else:
+                    lines.append("%d track(s) could not be verified "
+                                 "as accurate" %
+                                 (nonHTOA - self._accuratelyRipped))
                 lines.append("")
                 lines.append("Some tracks could not be verified as accurate")
-                '''
-                The following line must be kept commented out to stick strictly
-                 to the way EAC treats AccurateRip's CRCs mismatch
-                '''
-                # self._errors = True
             else:
                 lines.append("All tracks accurately ripped")
-
         lines.append("")
+
+        # FIXME: ATM this will always pick else (when does EAC report errors)
         if self._errors:
             lines.append("There were errors")
         else:
             lines.append("No errors occurred")
         lines.append("")
+
+        # END of ripper status report
+        # in EAC this isn't always the second to last line in the log because
+        # plugins information are included beneath (but before log checksum)
         lines.append("End of status report")
         lines.append("")
 
+        # Log checksum (uppercase hex encoded SHA256 hash of all lines)
+        # It isn't compatible with EAC's one: checklog fail
         hasher = hashlib.sha256()
         hasher.update("\n".join(lines).encode("utf-8"))
-        lines.append("==== Log checksum %s ====" % hasher.hexdigest())
+        lines.append("==== Log checksum %s ====" % hasher.hexdigest().upper())
         lines.append("")
 
         return lines
 
     def trackLog(self, trackResult):
+        """Returns tracks section lines: data picked from trackResult"""
 
         lines = []
 
-        lines.append('Track %2d' % trackResult.number)
-        lines.append('')
-        lines.append('     Filename %s' % trackResult.filename)
-        lines.append('')
+        # Track number (formatting like EAC's one)
+        if trackResult.number < 10:
+            lines.append("Track  %2d" % trackResult.number)
+        else:
+            lines.append("Track %2d" % trackResult.number)
+        lines.append("")
 
-        # EAC adds the 2 seconds to the first track pregap
+        # Filename (including path) of ripped track
+        lines.append("     Filename %s" % trackResult.filename)
+        lines.append("")
+
+        # Pre-gap length
+        # EAC always adds 2 seconds to the first track pre-gap
         pregap = trackResult.pregap
         if trackResult.number == 1:
             pregap += 2 * common.FRAMES_PER_SECOND
         if pregap:
-            lines.append('     Pre-gap length %s' % self._framesToHMSH(
+            lines.append("     Pre-gap length  %s" % self._framesToHMSF(
                 pregap))
-            lines.append('')
+            lines.append("")
 
+        # Peak level
         # EAC seems to format peak differently, truncating to the 3rd digit,
         # and also calculating it against a max of 32767
         # MBV - Feed me with your kiss: replaygain 0.809875,
         # EAC's peak level 80.9 % instead of 90.0 %
         peak = trackResult.peak * 32768 / 32767
-        # lines.append('     Peak level %r' % peak)
-        lines.append('     Peak level %.1f %%' % (
+        lines.append("     Peak level %.1f %%" % (
             int(peak * 1000) / 10.0))
-        # level = "%.2f" % (trackResult.peak * 100.0)
-        # level = level[:-1]
-        # lines.append('     Peak level %s %%' % level)
+        
+        # Extraction speed
         if trackResult.copyspeed:
-            lines.append('     Extraction speed %.1f X' % (
+            lines.append("     Extraction speed %.1f X" % (
                 trackResult.copyspeed))
-        # Track quality is shown in secure mode
+        
+        # Track quality
         if trackResult.quality and trackResult.quality > 0.001:
-            lines.append('     Track quality %.1f %%' % (
+            lines.append("     Track quality %.1f %%" % (
                 trackResult.quality * 100.0, ))
+        
+        # Ripper test CRC
         if trackResult.testcrc is not None:
-            lines.append('     Test CRC %08X' % trackResult.testcrc)
+            lines.append("     Test CRC %08X" % trackResult.testcrc)
+        
+        # Ripper copy CRC
         if trackResult.copycrc is not None:
-            lines.append('     Copy CRC %08X' % trackResult.copycrc)
+            lines.append("     Copy CRC %08X" % trackResult.copycrc)
 
+        # AccurateRip track status
+        # ATM there's no support for AccurateRip V2
         if trackResult.accurip:
             self._inARDatabase += 1
             if trackResult.ARCRC == trackResult.ARDBCRC:
                 lines.append(
-                    '     Accurately ripped (confidence %d)  [%08X]  (AR v1)' %
+                    "     Accurately ripped (confidence %d)  [%08X]  (AR v1)" %
                     (trackResult.ARDBConfidence, trackResult.ARCRC))
                 self._accuratelyRipped += 1
             else:
                 lines.append(
-                    '     Cannot be verified as accurate  '
-                    '(confidence %d),  [%08X], AccurateRip '
-                    'returned [%08x]  (AR v1)' %
+                    "     Cannot be verified as accurate  "
+                    "(confidence %d),  [%08X], AccurateRip "
+                    "returned [%08x]  (AR v1)" %
                     (trackResult.ARDBConfidence,
                      trackResult.ARCRC, trackResult.ARDBCRC))
         else:
-            lines.append('     Track not present in AccurateRip database')
+            lines.append("     Track not present in AccurateRip database")
 
+        # EAC emits 0 warnings even when a CRC mismatch occurs
         if trackResult.testcrc == trackResult.copycrc:
-            lines.append('     Copy OK')
-        # FIXME: else ?
+            lines.append("     Copy OK")
         return lines

commit 914c2170d955e869a3ae9fc73e5ce095929377c0

Fix regression
I haven't tested it but this commit should fix two regressions introduced in 983e1da208925dccd9933810019c44727111fe89.

eaclogger/logger/eac.py
@@ -187,6 +187,13 @@ class EacLogger(result.Logger):
         lines.append("")
         lines.append("")
 
+        # per-track
+        for t in ripResult.tracks:
+            if not t.filename:
+                continue
+            lines.extend(self.trackLog(t))
+            lines.append('')
+
         # AccurateRip summary at the end of the logfile
         if self._inARDatabase == 0:
             lines.append("None of the tracks are present "
@@ -201,27 +208,18 @@ class EacLogger(result.Logger):
                     "You may have a different pressing "
                     "from the one(s) in the database")
             elif self._accuratelyRipped < nonHTOA:
-                if self._accuratelyRipped < 10:
-                    lines.append(" %d track(s) accurately ripped" %
-                                 self._accuratelyRipped)
-                else:
-                    lines.append("%d track(s) accurately ripped" %
-                                 self._accuratelyRipped)
-                if (nonHTOA - self._accuratelyRipped) < 10:
-                    lines.append(" %d track(s) could not be verified "
-                                 "as accurate" %
-                                 (nonHTOA - self._accuratelyRipped))
-                else:
-                    lines.append("%d track(s) could not be verified "
-                                 "as accurate" %
-                                 (nonHTOA - self._accuratelyRipped))
+                lines.append("%d track(s) accurately ripped" %
+                             self._accuratelyRipped)
+                lines.append("%d track(s) could not be verified as accurate" %
+                             (nonHTOA - self._accuratelyRipped))
                 lines.append("")
                 lines.append("Some tracks could not be verified as accurate")
+
             else:
                 lines.append("All tracks accurately ripped")
         lines.append("")
 
-        # FIXME: ATM this will always pick else (when does EAC report errors)
+        # FIXME: ATM this will always pick else (when does EAC report errors?)
         if self._errors:
             lines.append("There were errors")
         else:
@@ -297,7 +295,7 @@ class EacLogger(result.Logger):
             lines.append("     Copy CRC %08X" % trackResult.copycrc)
 
         # AccurateRip track status
-        # ATM there's no support for AccurateRip V2
+        # No support for AccurateRip V2 until whipper 0.5.1
         if trackResult.accurip:
             self._inARDatabase += 1
             if trackResult.ARCRC == trackResult.ARDBCRC:

setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="morituri-eaclogger",
-    version="0.2.3",
+    version="0.2.4",
     description="""morituri EAC-style logger""",
     author="superveloman",
     maintainer="JoeLametta",